e22dae934ba508a0c5cd6ffb2d58f7cbf2333ddf
Donovan Messer <89869979+donomess@users.noreply.github.com>
2021-11-30 22:44:56 -0500

    Update DijkstraAlgorithm_OpenACC.c
    removed extra loop. but this still doesn't compile :(

--- DijkstraAlgorithm_OpenACC.c
+++ DijkstraAlgorithm_OpenACC.c
@@ -43,7 +43,6 @@ void dijkstra(int graph[V][V], int src)
 for (int count = 0; count < V - 1; count++) {
         int u = minDistance(dist, sptSet);
         sptSet[u] = true;
-        #pragma acc parallel loop
         for (int v = 0; v < V; v++){
           if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX
                 && dist[u] + graph[u][v] < dist[v])
